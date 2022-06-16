645cb2af16a4f8056043f01b5ca313ca923e94c9
Alexey Kurepin <kurepin@tut.by>
2022-06-16 22:19:01 +0300

    Пофиксил представление

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -149,7 +149,9 @@ class CommentsViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        review = get_object_or_404(title.reviews,
+                                   pk=self.kwargs.get('review_id'))
         queryset = review.comments.all()
         return queryset
 
