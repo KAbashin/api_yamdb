491979b48d04f37c0ddeaf291ee564ed55a731a2
Alexey Kurepin <kurepin@tut.by>
2022-06-19 22:56:19 +0300

    Серые метки исправил

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,3 @@
-from api_yamdb.settings import DEFAULT_FROM_EMAIL
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django_filters.rest_framework import DjangoFilterBackend
@@ -176,8 +175,7 @@ class ReviewsViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
-        queryset = title.reviews.all()
-        return queryset
+        return title.reviews.all()
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
@@ -200,6 +198,7 @@ class CommentsViewSet(viewsets.ModelViewSet):
         return queryset
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'),
+                                   title_id=self.kwargs.get('title_id'))
         user = self.request.user
         serializer.save(author=user, review=review)
